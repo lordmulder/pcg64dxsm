2aa529a3d37128c2b6c045bba65100fa2ce13ead
MuldeR <mulder2@gmx.de>
2026-04-04 21:02:53 +0200

    Added simple build script for Linux.

---
+++ .build/linux/mk-docker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -eo pipefail
+cd -- "$(realpath -- "$(dirname -- "${BASH_SOURCE[0]}")")"
+
+unset RUSTC_BOOTSTRAP
+readonly IMAGE_SPEC=lordmulder/rust-xbuild:1.94-trixie-r3
+
+set -x
+exec docker run --rm -v "${PWD}/../..":/workspace:ro -v "${PWD}/out":/workspace/.build/linux/out --tmpfs /tmp/rust-build:rw,exec -w /workspace "${IMAGE_SPEC}" make -C .build/linux
