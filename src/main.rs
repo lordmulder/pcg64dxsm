9e5afbd4723cbd2f5dd4387362c9e204e8309168
MuldeR <mulder2@gmx.de>
2026-04-05 22:17:30 +0200

    Fixed a panic that could occur when options '--hex' and '--count' are used together.

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,11 @@ use clap::Parser;
 use hex::encode_to_slice;
 use hex_literal::hex;
 use hkdf::Hkdf;
-use parking_lot::{Condvar, Mutex};
-use rand_pcg::{
-    Lcg128CmDxsm64, Mcg128Xsl64,
-    rand_core::{Rng, SeedableRng},
-};
+use rand_pcg::{Lcg128CmDxsm64, Mcg128Xsl64, rand_core::SeedableRng};
 use sha3::Sha3_256;
 use std::{
-    io::{Error as IoError, StdoutLock, Write, stdout},
+    io::{Error as IoError, Write, stdout},
     mem::MaybeUninit,
-    sync::atomic::{AtomicBool, Ordering},
-    thread,
 };
 
 /// Supported random number generators
@@ -24,6 +18,9 @@ enum Generator {
     Pcg64Dxsm(Lcg128CmDxsm64),
     Pcg64Mcg(Mcg128Xsl64),
 }
+
+/// Buffer size
+const BUFF_SIZE: usize = 8192usize;
 
 // ===========================================================================
 // Utilities
@@ -67,151 +64,168 @@ fn write_raw(output: &mut dyn Write, data: &[u8]) -> Result<(), IoError> {
 fn write_hex(output: &mut dyn Write, data: &[u8]) -> Result<(), IoError> {
     let mut array: MaybeUninit<[u8; 2usize * BUFF_SIZE]> = MaybeUninit::uninit();
     let hex_buffer = unsafe { array.assume_init_mut() };
-    encode_to_slice(data, hex_buffer).unwrap();
-    output.write_all(&hex_buffer[..(2usize * data.len())])
+    let hex_length = data.len().checked_mul(2usize).unwrap();
+    encode_to_slice(data, &mut hex_buffer[..hex_length]).unwrap();
+    output.write_all(&hex_buffer[..hex_length])
 }
 
 // ===========================================================================
 // MT Generator
 // ===========================================================================
 
-const BUFF_SIZE: usize = 8192usize;
-const NUM_BUFFERS: usize = 16usize;
+mod mt {
+    use super::{BUFF_SIZE, remaining};
+    use parking_lot::{Condvar, Mutex};
+    use rand_pcg::rand_core::Rng;
+    use std::{
+        io::{Error as IoError, StdoutLock, Write},
+        sync::atomic::{AtomicBool, Ordering},
+        thread,
+    };
 
-static RUNNING: AtomicBool = AtomicBool::new(true);
+    const NUM_BUFFERS: usize = 16usize;
 
-static BUFFER: [Mutex<(bool, [u8; BUFF_SIZE])>; NUM_BUFFERS] = [
-    Mutex::new((false, [0u8; _])),
-    Mutex::new((false, [0u8; _])),
-    Mutex::new((false, [0u8; _])),
-    Mutex::new((false, [0u8; _])),
-    Mutex::new((false, [0u8; _])),
-    Mutex::new((false, [0u8; _])),
-    Mutex::new((false, [0u8; _])),
-    Mutex::new((false, [0u8; _])),
-    Mutex::new((false, [0u8; _])),
-    Mutex::new((false, [0u8; _])),
-    Mutex::new((false, [0u8; _])),
-    Mutex::new((false, [0u8; _])),
-    Mutex::new((false, [0u8; _])),
-    Mutex::new((false, [0u8; _])),
-    Mutex::new((false, [0u8; _])),
-    Mutex::new((false, [0u8; _])),
-];
+    static RUNNING: AtomicBool = AtomicBool::new(true);
 
-static COND_FREE: [Condvar; NUM_BUFFERS] = [
-    Condvar::new(),
-    Condvar::new(),
-    Condvar::new(),
-    Condvar::new(),
-    Condvar::new(),
-    Condvar::new(),
-    Condvar::new(),
-    Condvar::new(),
-    Condvar::new(),
-    Condvar::new(),
-    Condvar::new(),
-    Condvar::new(),
-    Condvar::new(),
-    Condvar::new(),
-    Condvar::new(),
-    Condvar::new(),
-];
+    static BUFFER: [Mutex<(bool, [u8; BUFF_SIZE])>; NUM_BUFFERS] = [
+        Mutex::new((false, [0u8; _])),
+        Mutex::new((false, [0u8; _])),
+        Mutex::new((false, [0u8; _])),
+        Mutex::new((false, [0u8; _])),
+        Mutex::new((false, [0u8; _])),
+        Mutex::new((false, [0u8; _])),
+        Mutex::new((false, [0u8; _])),
+        Mutex::new((false, [0u8; _])),
+        Mutex::new((false, [0u8; _])),
+        Mutex::new((false, [0u8; _])),
+        Mutex::new((false, [0u8; _])),
+        Mutex::new((false, [0u8; _])),
+        Mutex::new((false, [0u8; _])),
+        Mutex::new((false, [0u8; _])),
+        Mutex::new((false, [0u8; _])),
+        Mutex::new((false, [0u8; _])),
+    ];
 
-static COND_USED: [Condvar; NUM_BUFFERS] = [
-    Condvar::new(),
-    Condvar::new(),
-    Condvar::new(),
-    Condvar::new(),
-    Condvar::new(),
-    Condvar::new(),
-    Condvar::new(),
-    Condvar::new(),
-    Condvar::new(),
-    Condvar::new(),
-    Condvar::new(),
-    Condvar::new(),
-    Condvar::new(),
-    Condvar::new(),
-    Condvar::new(),
-    Condvar::new(),
-];
+    static COND_FREE: [Condvar; NUM_BUFFERS] = [
+        Condvar::new(),
+        Condvar::new(),
+        Condvar::new(),
+        Condvar::new(),
+        Condvar::new(),
+        Condvar::new(),
+        Condvar::new(),
+        Condvar::new(),
+        Condvar::new(),
+        Condvar::new(),
+        Condvar::new(),
+        Condvar::new(),
+        Condvar::new(),
+        Condvar::new(),
+        Condvar::new(),
+        Condvar::new(),
+    ];
 
-fn generate_mt<F>(mut generator: impl Rng + Send + 'static, mut output: StdoutLock, write_fn: F, count: Option<u64>)
-where
-    F: Fn(&mut dyn Write, &[u8]) -> Result<(), IoError>,
-{
-    let mut bytes_written = 0u64;
+    static COND_USED: [Condvar; NUM_BUFFERS] = [
+        Condvar::new(),
+        Condvar::new(),
+        Condvar::new(),
+        Condvar::new(),
+        Condvar::new(),
+        Condvar::new(),
+        Condvar::new(),
+        Condvar::new(),
+        Condvar::new(),
+        Condvar::new(),
+        Condvar::new(),
+        Condvar::new(),
+        Condvar::new(),
+        Condvar::new(),
+        Condvar::new(),
+        Condvar::new(),
+    ];
 
-    let handle = thread::spawn(move || {
-        while RUNNING.load(Ordering::Relaxed) {
-            for i in 0usize..NUM_BUFFERS {
-                let mut buffer = BUFFER[i].lock();
-                while buffer.0 {
-                    COND_FREE[i].wait(&mut buffer);
+    pub fn generate<F>(mut generator: impl Rng + Send + 'static, mut output: StdoutLock, write_fn: F, count: Option<u64>)
+    where
+        F: Fn(&mut dyn Write, &[u8]) -> Result<(), IoError>,
+    {
+        let mut bytes_written = 0u64;
+
+        let handle = thread::spawn(move || {
+            while RUNNING.load(Ordering::Relaxed) {
+                for i in 0usize..NUM_BUFFERS {
+                    let mut buffer = BUFFER[i].lock();
+                    while buffer.0 {
+                        COND_FREE[i].wait(&mut buffer);
+                    }
+                    generator.fill_bytes(&mut buffer.1);
+                    buffer.0 = true;
+                    COND_USED[i].notify_one();
                 }
-                generator.fill_bytes(&mut buffer.1);
-                buffer.0 = true;
-                COND_USED[i].notify_one();
+            }
+        });
+
+        'out_loop: loop {
+            for i in 0usize..NUM_BUFFERS {
+                let chunk_size = count.map(|total_bytes| remaining(total_bytes, bytes_written)).unwrap_or(BUFF_SIZE);
+                if chunk_size == 0usize {
+                    break 'out_loop;
+                }
+                let mut buffer = BUFFER[i].lock();
+                while !buffer.0 {
+                    COND_USED[i].wait(&mut buffer);
+                }
+                if write_fn(&mut output, &buffer.1[..chunk_size]).is_err() {
+                    break 'out_loop;
+                }
+                bytes_written += chunk_size as u64;
+                buffer.0 = false;
+                COND_FREE[i].notify_one();
             }
         }
-    });
 
-    'out_loop: loop {
+        RUNNING.store(false, Ordering::Relaxed);
+
         for i in 0usize..NUM_BUFFERS {
-            let chunk_size = count.map(|total_bytes| remaining(total_bytes, bytes_written)).unwrap_or(BUFF_SIZE);
-            if chunk_size == 0usize {
-                break 'out_loop;
-            }
             let mut buffer = BUFFER[i].lock();
-            while !buffer.0 {
-                COND_USED[i].wait(&mut buffer);
-            }
-            if write_fn(&mut output, &buffer.1[..chunk_size]).is_err() {
-                break 'out_loop;
-            }
-            bytes_written += chunk_size as u64;
             buffer.0 = false;
             COND_FREE[i].notify_one();
         }
+
+        handle.join().unwrap();
     }
-
-    RUNNING.store(false, Ordering::Relaxed);
-
-    for i in 0usize..NUM_BUFFERS {
-        let mut buffer = BUFFER[i].lock();
-        buffer.0 = false;
-        COND_FREE[i].notify_one();
-    }
-
-    handle.join().unwrap();
 }
 
 // ===========================================================================
 // ST Generator
 // ===========================================================================
 
-fn generate_st<F>(mut generator: impl Rng, mut output: StdoutLock, write_fn: F, count: Option<u64>)
-where
-    F: Fn(&mut dyn Write, &[u8]) -> Result<(), IoError>,
-{
-    let mut bytes_written = 0u64;
-    let mut buffer = [0u8; BUFF_SIZE];
+mod st {
+    use super::{BUFF_SIZE, remaining};
+    use rand_pcg::rand_core::Rng;
+    use std::io::{Error as IoError, StdoutLock, Write};
 
-    if let Some(total_bytes) = count {
-        while bytes_written < total_bytes {
-            let chunk_size = remaining(total_bytes, bytes_written);
-            generator.fill_bytes(&mut buffer[..chunk_size]);
-            if write_fn(&mut output, &buffer[..chunk_size]).is_err() {
-                break;
+    pub fn generate<F>(mut generator: impl Rng, mut output: StdoutLock, write_fn: F, count: Option<u64>)
+    where
+        F: Fn(&mut dyn Write, &[u8]) -> Result<(), IoError>,
+    {
+        let mut bytes_written = 0u64;
+        let mut buffer = [0u8; BUFF_SIZE];
+
+        if let Some(total_bytes) = count {
+            while bytes_written < total_bytes {
+                let chunk_size = remaining(total_bytes, bytes_written);
+                generator.fill_bytes(&mut buffer[..chunk_size]);
+                if write_fn(&mut output, &buffer[..chunk_size]).is_err() {
+                    break;
+                }
+                bytes_written += chunk_size as u64;
             }
-            bytes_written += chunk_size as u64;
-        }
-    } else {
-        loop {
-            generator.fill_bytes(&mut buffer);
-            if write_fn(&mut output, &buffer).is_err() {
-                break;
+        } else {
+            loop {
+                generator.fill_bytes(&mut buffer);
+                if write_fn(&mut output, &buffer).is_err() {
+                    break;
+                }
             }
         }
     }
@@ -264,25 +278,25 @@ fn main() {
     if args.thread {
         if !args.hex {
             match generator {
-                Generator::Pcg64Dxsm(pcg64dxsm) => generate_mt(pcg64dxsm, output, write_raw, args.count),
-                Generator::Pcg64Mcg(pcg64mcg) => generate_mt(pcg64mcg, output, write_raw, args.count),
+                Generator::Pcg64Dxsm(pcg64dxsm) => mt::generate(pcg64dxsm, output, write_raw, args.count),
+                Generator::Pcg64Mcg(pcg64mcg) => mt::generate(pcg64mcg, output, write_raw, args.count),
             }
         } else {
             match generator {
-                Generator::Pcg64Dxsm(pcg64dxsm) => generate_mt(pcg64dxsm, output, write_hex, args.count),
-                Generator::Pcg64Mcg(pcg64mcg) => generate_mt(pcg64mcg, output, write_hex, args.count),
+                Generator::Pcg64Dxsm(pcg64dxsm) => mt::generate(pcg64dxsm, output, write_hex, args.count),
+                Generator::Pcg64Mcg(pcg64mcg) => mt::generate(pcg64mcg, output, write_hex, args.count),
             }
         }
     } else {
         if !args.hex {
             match generator {
-                Generator::Pcg64Dxsm(pcg64dxsm) => generate_st(pcg64dxsm, output, write_raw, args.count),
-                Generator::Pcg64Mcg(pcg64mcg) => generate_st(pcg64mcg, output, write_raw, args.count),
+                Generator::Pcg64Dxsm(pcg64dxsm) => st::generate(pcg64dxsm, output, write_raw, args.count),
+                Generator::Pcg64Mcg(pcg64mcg) => st::generate(pcg64mcg, output, write_raw, args.count),
             }
         } else {
             match generator {
-                Generator::Pcg64Dxsm(pcg64dxsm) => generate_st(pcg64dxsm, output, write_hex, args.count),
-                Generator::Pcg64Mcg(pcg64mcg) => generate_st(pcg64mcg, output, write_hex, args.count),
+                Generator::Pcg64Dxsm(pcg64dxsm) => st::generate(pcg64dxsm, output, write_hex, args.count),
+                Generator::Pcg64Mcg(pcg64mcg) => st::generate(pcg64mcg, output, write_hex, args.count),
             }
         }
     }
